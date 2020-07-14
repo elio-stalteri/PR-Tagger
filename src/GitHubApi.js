1e8d4fed4cac66721ee9fe6b67de2d53a811b4a2
Elio <elio.stalteri@clientrelationship.com>
2020-07-14 13:17:57 +0100

    Related #1

--- src/GitHubApi.js
+++ src/GitHubApi.js
@@ -1,25 +1,51 @@
-const BaseUrl = "https://api.github.com"
+const BaseUrl = "https://api.github.com";
+
+const linkTypes = ["prev", "next", "last", "first"];
+
+const getPagesConfig = function (resp) {
+  const linksHeader = resp.headers.get("link");
+  if (linksHeader) {
+    let links = linksHeader.split(",");
+    links = linkTypes.reduce((acc, linkType) => {
+      const pageLink = links.filter(
+        (link) => link.indexOf(`rel="${linkType}"`) > -1
+      );
+      if (pageLink.length > 0) {
+        return {
+          ...acc,
+          [linkType]: parseInt(pageLink[0].split("page=")[1].split(">")[0]),
+        };
+      }
+      return { ...acc, [linkType]: null };
+    }, {});
+    return links;
+  }
+  return linkTypes.reduce((acc, linkType) => {
+    return { ...acc, [linkType]: null };
+  }, {});
+};
 
 export const getRepoComments = async function (owner, repo) {
   const resp = await fetch(
-    `${BaseUrl}/repos/${owner}/${repo}/pulls/comments?sort&direction=asc`,
+    `${BaseUrl}/repos/${owner}/${repo}/pulls/comments?sort&direction=asc&page=1&per_page=100`,
     {
       headers: {
         Authorization: " token " + localStorage.getItem("GithubLogInTocken"),
       },
     }
   );
+  const links = getPagesConfig(resp);
+  console.log("comments links:", links);
   return await resp.json();
-
 };
 
-
 export const getRepos = async function (owner) {
-  const resp = await fetch(`${BaseUrl}/orgs/${owner}/repos`, {
+  const resp = await fetch(`${BaseUrl}/orgs/${owner}/repos?page=1&per_page=100`, {
     headers: {
       Authorization: " token " + localStorage.getItem("GithubLogInTocken"),
     },
   });
+  const links = getPagesConfig(resp);
+  console.log("repos links:", links);
   return await resp.json();
 };
-
